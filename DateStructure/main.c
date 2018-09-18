49a892196c7a363fadf44cf3b2cecad24b047b29
barzarrhey's mac <m1812882555@hotmail.com>
2018-09-18 09:07:29 +0800

    Initial Commit

---
+++ DateStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DateStructure
+//
+//  Created by Barzarrhey on 2018/9/18.
+//  Copyright © 2018年 Barzarrhey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
